4695a0db30bf3418663df79f56fda5fe94acb0f8
kim2629 <74346292+kimjk2629@users.noreply.github.com>
2026-08-29 11:48:52 +0900

    Refactor CarState and CarController for Tesla

--- opendbc_repo/opendbc/car/tesla/carstate.py
+++ opendbc_repo/opendbc/car/tesla/carstate.py
@@ -1,106 +1,259 @@
-import numpy as np
-from opendbc.can import CANPacker
-from opendbc.car import Bus, apply_steer_angle_limits_vm, structs
-from opendbc.car.interfaces import CarControllerBase
-from opendbc.car.tesla.teslacan import TeslaCAN
-from opendbc.car.tesla.values import CarControllerParams, TeslaFlags
-from opendbc.car.tesla.coop_steering import CoopSteeringCarController
-from opendbc.car.vehicle_model import VehicleModel
+import copy
+from opendbc.can import CANDefine, CANParser
+from opendbc.car import Bus, create_button_events, structs
+from opendbc.car.carlog import carlog
+from opendbc.car.common.conversions import Conversions as CV
+from opendbc.car.interfaces import CarStateBase
+from opendbc.car.tesla.values import DBC, CANBUS, GEAR_MAP, STEER_THRESHOLD, TeslaFlags
+
+ButtonType = structs.CarState.ButtonEvent.Type
 
 
-def get_safety_CP():
-  # We use the TESLA_MODEL_Y platform for lateral limiting to match safety
-  # A Model 3 at 40 m/s using the Model Y limits sees a <0.3% difference in max angle (from curvature factor)
-  from opendbc.car.tesla.interface import CarInterface
-  return CarInterface.get_non_essential_params("TESLA_MODEL_Y")
+class CarState(CarStateBase):
+  def __init__(self, CP):
+    super().__init__(CP)
+    self.can_define = CANDefine(DBC[CP.carFingerprint][Bus.party])
+    self.shifter_values = self.can_define.dv["DI_systemStatus"]["DI_gear"]
 
+    self.summon = False
+    self.summon_prev = False
+    self.cruise_enabled_prev = False
+    self.fsd14_error_logged = False
+    self.suspected_fsd14 = False
+    self.suspected_fsd14_clear_frames = 0
 
-class CarController(CarControllerBase):
-  def __init__(self, dbc_names, CP):
-    super().__init__(dbc_names, CP)
-    self.apply_angle_last = 0
-    self.packer = CANPacker(dbc_names[Bus.party])
-    self.tesla_can = TeslaCAN(CP, self.packer)
+    self.hands_on_level = 0
+    self.acc_cancel_last = 0
+    self.das_control = None
+    self.das_body_controls_dat = b""
+    self.das_accCancel = False
+    self.cruise_override = False
     self.coop_steering = True
-    self.coop_steer = CoopSteeringCarController()
+    self.infotainment_3_finger_press = 0
 
-    # Vehicle model used for lateral limiting
-    self.VM = VehicleModel(get_safety_CP())
+  def update_summon_state(self, summon_state: str, cruise_enabled: bool):
+    summon_now = summon_state in ("ACTIVE", "COMPLETE", "SELFPARK_STARTED")
+    if summon_now and not self.summon_prev and not self.cruise_enabled_prev:
+      self.summon = True
+    if not summon_now:
+      self.summon = False
+    self.summon_prev = summon_now
+    self.cruise_enabled_prev = cruise_enabled
 
-    # Blinker MITM state
-    self.has_vehicle_bus = bool(CP.flags & TeslaFlags.HAS_VEHICLE_BUS)
-    self.body_controls_counter_last = -1
-    self.blinker_request_prev = False
-    self.blinker_cancel_frame = 0
+  def update(self, can_parsers) -> structs.CarState:
+    cp_party = can_parsers[Bus.party]
+    cp_ap_party = can_parsers[Bus.ap_party]
+    ret = structs.CarState()
+    length = 0.11
 
-  def update(self, CC, CS, now_nanos):
-    actuators = CC.actuators
-    can_sends = []
+    # Vehicle speed
+    ret.vEgoRaw = cp_party.vl["DI_speed"]["DI_vehicleSpeed"] * CV.KPH_TO_MS
+    ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
 
-    # Tesla EPS enforces disabling steering on heavy lateral override force.
-    # When enabling in a tight curve, we wait until user reduces steering force to start steering.
-    # Canceling is done on rising edge and is handled generically with CC.cruiseControl.cancel
-    lat_active = CC.latActive and CS.hands_on_level < 3
+    # Wheel speeds (km/h -> m/s)
+    ws = cp_party.vl["ESP_wheelSpeeds"]
+    ret.wheelSpeeds.fl = ws["ESP_wheelSpeedFrL"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.fr = ws["ESP_wheelSpeedFrR"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.rl = ws["ESP_wheelSpeedReL"] * CV.KPH_TO_MS
+    ret.wheelSpeeds.rr = ws["ESP_wheelSpeedReR"] * CV.KPH_TO_MS
 
-    if self.frame % CarControllerParams.STEER_STEP == 0:
-      # Angular rate limit based on speed
-      self.apply_angle_last = apply_steer_angle_limits_vm(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw, CS.out.steeringAngleDeg,
-                                                          lat_active, CarControllerParams, self.VM)
+    # Displayed speed
+    ui_speed_units_raw = int(cp_party.vl["DI_speed"]["DI_uiSpeedUnits"])
+    ui_speed_units = self.can_define.dv.get("DI_speed", {}).get("DI_uiSpeedUnits", {}).get(ui_speed_units_raw, ui_speed_units_raw)
+    ui_speed = cp_party.vl["DI_speed"]["DI_uiSpeed"]
 
-      if self.coop_steering:
-        coop_result = self.coop_steer.update(self.apply_angle_last, lat_active, CS, self.VM)
-        can_sends.append(self.tesla_can.create_steering_control(coop_result.steeringAngleDeg, coop_result.lat_active,
-                                                                 (self.frame // CarControllerParams.STEER_STEP) % 16))
-      else:
-        can_sends.append(self.tesla_can.create_steering_control(self.apply_angle_last, lat_active,
-                                                                 (self.frame // CarControllerParams.STEER_STEP) % 16))
-
-    if self.frame % 10 == 0:
-      can_sends.append(self.tesla_can.create_steering_allowed((self.frame // 10) % 16))
-
-    # Longitudinal control
-    if self.CP.openpilotLongitudinalControl:
-      if self.frame % 4 == 0:
-        state = 13 if CC.cruiseControl.cancel or CS.das_accCancel else 4  # 4=ACC_ON, 13=ACC_CANCEL_GENERIC_SILENT
-        accel = float(np.clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX))
-        if not CC.longActive:
-          accel = 0.
-        cntr = (self.frame // 4) % 8
-        set_speed_kph = None  # TODO: pass from params when available
-        can_sends.append(self.tesla_can.create_longitudinal_command(state, accel, cntr, CS.out.vEgo, CC.longActive,
-                                                                    CS.cruise_override, set_speed_kph=set_speed_kph))
+    # Infer display unit from consistency with wheel speed first, then fall back to CAN enum/raw bit.
+    ui_is_kph = False
+    if ret.vEgoRaw > 2.0 and ui_speed > 2.0:
+      ui_speed_kph_ms = ui_speed * CV.KPH_TO_MS
+      ui_speed_mph_ms = ui_speed * CV.MPH_TO_MS
+      ui_is_kph = abs(ui_speed_kph_ms - ret.vEgoRaw) <= abs(ui_speed_mph_ms - ret.vEgoRaw)
+    elif ui_speed_units in ("DI_SPEED_KPH", "KPH"):
+      ui_is_kph = True
+    elif ui_speed_units in ("DI_SPEED_MPH", "MPH"):
+      ui_is_kph = False
     else:
-      # Increment counter so cancel is prioritized even without openpilot longitudinal
-      if CC.cruiseControl.cancel:
-        cntr = (CS.das_control["DAS_controlCounter"] + 1) % 8
-        can_sends.append(self.tesla_can.create_longitudinal_command(13, 0, cntr, CS.out.vEgo, False, True))
+      ui_is_kph = ui_speed_units_raw == 1
 
-    # Nav blinker control via DAS_bodyControls on the vehicle bus, phase-locked to the car's
-    # counter. Cancel on the trailing edge since the body controller latches the signal.
-    stock_dat = getattr(CS, 'das_body_controls_dat', b"")
-    if self.has_vehicle_bus and len(stock_dat) >= 8:
-      left_blinker = CC.leftBlinker
-      right_blinker = CC.rightBlinker
+    ret.vEgoCluster = ui_speed * (CV.KPH_TO_MS if ui_is_kph else CV.MPH_TO_MS)
 
-      driver_opposes = (left_blinker and CS.out.rightBlinker) or (right_blinker and CS.out.leftBlinker)
-      if driver_opposes:
-        left_blinker = right_blinker = False
+    # Gas pedal
+    pedal_status = cp_party.vl["DI_systemStatus"]["DI_accelPedalPos"]
+    ret.gas = pedal_status / 100.0
+    ret.gasPressed = pedal_status > 0
 
-      nav_requesting = left_blinker or right_blinker
+    # Motor speed (EV: motor RPM from inverter)
+    ret.engineRpm = cp_party.vl["DI_torque"]["DI_axleSpeed"]
 
-      if self.blinker_request_prev and not nav_requesting and not driver_opposes:
-        self.blinker_cancel_frame = self.frame + 150  # ~1.5 s
-      self.blinker_request_prev = nav_requesting
-      cancel = not nav_requesting and not driver_opposes and self.frame < self.blinker_cancel_frame
+    # Brake pedal. Newer Model Y vehicles do not expose IBST_status on the party bus,
+    # while ESP_status is available across the supported Model 3/Y platforms.
+    ret.brake = 0.0
+    ret.brakePressed = cp_party.vl["ESP_status"]["ESP_driverBrakeApply"] == 2
+    ret.brakeLights = cp_party.vl["ESP_status"]["ESP_brakeLamp"] == 1
+    ret.regenBraking = cp_party.vl["DI_systemStatus"]["DI_regenLight"] != 0
+    ret.espDisabled = cp_party.vl["ESP_status"]["ESP_espFaultLamp"] != 0
+    ret.espActive = cp_party.vl["ESP_status"]["ESP_espModeActive"] != 0
 
-      body_counter = stock_dat[6] >> 4
-      if body_counter != self.body_controls_counter_last:
-        can_sends.append(self.tesla_can.create_body_controls(stock_dat, left_blinker, right_blinker, cancel))
-      self.body_controls_counter_last = body_counter
+    # Steering wheel
+    epas_status = cp_party.vl["EPAS3S_sysStatus"]
+    self.hands_on_level = epas_status["EPAS3S_handsOnLevel"]
+    ret.steeringAngleDeg = -epas_status["EPAS3S_internalSAS"]
+    ret.steeringRateDeg = -cp_ap_party.vl["SCCM_steeringAngleSensor"]["SCCM_steeringAngleSpeed"]
+    ret.steeringTorque = -epas_status["EPAS3S_torsionBarTorque"]
+    ret.steeringTorqueEps = -epas_status["EPAS3S_steeringRackForce"] * length / self.CP.steerRatio
 
-    # TODO: HUD control
-    new_actuators = actuators.as_builder()
-    new_actuators.steeringAngleDeg = self.apply_angle_last
+    # Stock handsOnLevel uses >0.5 for 0.25s, but this threshold reacts faster.
+    ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > STEER_THRESHOLD, 5)
 
-    self.frame += 1
-    return new_actuators, can_sends
+    eac_status = self.can_define.dv["EPAS3S_sysStatus"]["EPAS3S_eacStatus"].get(int(epas_status["EPAS3S_eacStatus"]), None)
+    ret.steerFaultPermanent = eac_status == "EAC_FAULT"
+    ret.steerFaultTemporary = eac_status == "EAC_INHIBITED"
+
+    # Do not set vehicleSensorsInvalid from SCCM_steeringAngleValidity: refreshed
+    # Model Y vehicles report 0 while angle/rate remain valid. EPS faults are covered above.
+
+    # FSD disengages on strong user override (handsOnLevel >= 3) or high angle rate faults (fast override, high speed)
+    eac_error_code = self.can_define.dv["EPAS3S_sysStatus"]["EPAS3S_eacErrorCode"].get(int(epas_status["EPAS3S_eacErrorCode"]), None)
+    self.steering_disengage = self.hands_on_level >= 3 or (eac_status == "EAC_INHIBITED" and
+                                                           eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
+
+    # Cruise state
+    cruise_state = self.can_define.dv["DI_state"]["DI_cruiseState"].get(int(cp_party.vl["DI_state"]["DI_cruiseState"]), None)
+    speed_units_raw = int(cp_party.vl["DI_state"]["DI_speedUnits"])
+    speed_units = self.can_define.dv["DI_state"]["DI_speedUnits"].get(speed_units_raw, speed_units_raw)
+    acc_state = cp_ap_party.vl["DAS_control"]["DAS_accState"]
+    # Respect all stock DAS cancel states, not just ACC_CANCEL_GENERIC_SILENT(13).
+    # ELDA/ELK triggers ACC_CANCEL_GENERIC(0) which must also be forwarded.
+    self.das_accCancel = acc_state in (0, 1, 2, 12, 13, 14, 15)
+
+    summon_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
+    cruise_enabled = cruise_state in ("ENABLED", "STANDSTILL", "OVERRIDE", "PRE_FAULT", "PRE_CANCEL")
+    self.cruise_override = cruise_state == "OVERRIDE"
+    self.update_summon_state(summon_state, cruise_enabled)
+
+    # Match panda safety cruise engaged logic
+    ret.cruiseState.enabled = cruise_enabled and not self.summon
+    if speed_units in ("KPH", "DI_SPEED_KPH"):
+      cruise_is_kph = True
+    elif speed_units in ("MPH", "DI_SPEED_MPH"):
+      cruise_is_kph = False
+    else:
+      # Keep cruise unit consistent with displayed speed when enum/raw bit are unreliable.
+      cruise_is_kph = ui_is_kph
+
+    ret.cruiseState.speedCluster = cp_party.vl["DI_state"]["DI_digitalSpeed"] * (CV.KPH_TO_MS if cruise_is_kph else CV.MPH_TO_MS)
+    ret.cruiseState.speed = max(ret.cruiseState.speedCluster, 1e-3)
+    ret.cruiseState.available = cruise_state == "STANDBY" or ret.cruiseState.enabled
+    ret.cruiseState.standstill = False  # This needs to be false, since we can resume from stop without sending anything special
+    ret.standstill = cruise_state == "STANDSTILL"
+    ret.accFaulted = cruise_state == "FAULT"
+
+    # Emit a single cancel button event on the rising edge of any stock DAS cancel state.
+    # Feeding the raw DAS_accState enum would emit spurious "unknown" events for normal
+    # states and miss cancel codes other than 0/13 that das_accCancel already covers.
+    acc_cancel = 1 if self.das_accCancel else 0
+    ret.buttonEvents = [*create_button_events(acc_cancel, self.acc_cancel_last, {1: ButtonType.cancel})]
+    self.acc_cancel_last = acc_cancel
+
+    # DAS_fusedSpeedLimit from DBC is always in kph (scale=5). Do NOT apply ui_is_kph conversion.
+    speed_limit = cp_ap_party.vl["DAS_status"]["DAS_fusedSpeedLimit"]
+    if 0 < speed_limit <= 150:
+      ret.speedLimit = speed_limit
+
+    park_brake_state = self.can_define.dv["DI_state"]["DI_parkBrakeState"].get(int(cp_party.vl["DI_state"]["DI_parkBrakeState"]), None)
+    vehicle_hold_state = self.can_define.dv["DI_state"]["DI_vehicleHoldState"].get(int(cp_party.vl["DI_state"]["DI_vehicleHoldState"]), None)
+    ret.parkingBrake = park_brake_state == "APPLIED"
+    ret.brakeHoldActive = vehicle_hold_state == "STANDSTILL"
+
+    # Gear
+    ret.gearShifter = GEAR_MAP[self.can_define.dv["DI_systemStatus"]["DI_gear"].get(int(cp_party.vl["DI_systemStatus"]["DI_gear"]), "DI_GEAR_INVALID")]
+
+    # Doors
+    ret.doorOpen = cp_party.vl["UI_warning"]["anyDoorOpen"] == 1
+
+    # Blinkers
+    ret.leftBlinker = cp_party.vl["UI_warning"]["leftBlinkerBlinking"] in (1, 2)
+    ret.rightBlinker = cp_party.vl["UI_warning"]["rightBlinkerBlinking"] in (1, 2)
+
+    # High beam stalk used as generic toggle (openpilot convention)
+    ret.genericToggle = cp_party.vl["UI_warning"]["highBeam"] == 1
+
+    # Seatbelt
+    ret.seatbeltUnlatched = cp_party.vl["UI_warning"]["buckleStatus"] != 1
+
+    # Blindspot
+    ret.leftBlindspot = cp_ap_party.vl["DAS_status"]["DAS_blindSpotRearLeft"] != 0
+    ret.rightBlindspot = cp_ap_party.vl["DAS_status"]["DAS_blindSpotRearRight"] != 0
+
+    # Stock AEB from DAS — the only reliable collision avoidance signal.
+    # DAS_steeringControlType (EMERGENCY_LANE_KEEP) also triggers on ELDA
+    # (normal lane correction), so it's NOT used for disengagement.
+    # The Panda safety layer handles emergency steering forwarding at
+    # the physical level (safety_tesla.h).
+    ret.stockAeb = cp_ap_party.vl["DAS_control"]["DAS_aebEvent"] == 1
+    ret.stockFcw = cp_ap_party.vl["DAS_status"]["DAS_forwardCollisionWarning"] != 0
+
+    # LKAS
+    # On FSD 14+, ANGLE_CONTROL behavior changed to allow user winddown while actuating.
+    # Stock Autosteer should be off (includes FSD)
+    # TODO: find for TESLA_MODEL_X and HW2.5 vehicles
+    if not (self.CP.flags & TeslaFlags.MISSING_DAS_SETTINGS):
+      ret.invalidLkasSetting = cp_ap_party.vl["DAS_status"]["DAS_autopilotState"] not in (0, 1, 2)  # DISABLED, UNAVAILABLE, AVAILABLE
+
+      # Because we don't have FSD 14 detection outside of a set of FW, we should check if this FW is accidentally missing from FSD_14_FW
+      # 1. If in Autosteer or FSD, already caught by invalidLkasSetting
+      # 2. If in TACC and DAS ever sends ANGLE_CONTROL (1), we can infer it's trying to do LKAS on FSD 14+
+      # NOTE: Tesla's latest firmware changed ELDA (Emergency Lane Departure Assist) to use ANGLE_CONTROL (1)
+      # instead of EMERGENCY_LANE_KEEP (3). Exclude ELDA by checking eac_status so it doesn't latch suspected_fsd14.
+      eac_is_emergency = eac_status == "EMERGENCY_LANE_KEEP"
+      angle_control = cp_ap_party.vl["DAS_steeringControl"]["DAS_steeringControlType"] == 1 and not eac_is_emergency  # ANGLE_CONTROL, excluding ELDA
+      if not ret.invalidLkasSetting and angle_control and not self.CP.flags & TeslaFlags.FSD_14:
+        self.suspected_fsd14 = True
+        self.suspected_fsd14_clear_frames = 0
+
+      if self.suspected_fsd14:
+        ret.invalidLkasSetting = True
+        if not self.fsd14_error_logged:
+          carlog.error("FSD 14 detected, but FW not in FSD_14_FW set")
+          self.fsd14_error_logged = True
+        # Un-latch if ANGLE_CONTROL has been absent for ~3 s (100 frames @ ~33 Hz).
+        # This allows re-engagement after transient triggers (e.g. if ELDA slips through on new FW variants).
+        if not angle_control:
+          self.suspected_fsd14_clear_frames += 1
+          if self.suspected_fsd14_clear_frames >= 100:
+            self.suspected_fsd14 = False
+            self.suspected_fsd14_clear_frames = 0
+        else:
+          self.suspected_fsd14_clear_frames = 0
+
+    # Buttons # ToDo: add Gap adjust button
+
+    # Messages needed by carcontroller
+    self.das_control = copy.copy(cp_ap_party.vl["DAS_control"])
+
+    # Raw stock DAS_bodyControls bytes (bus 2), used to ride the blinker on the vehicle bus.
+    if Bus.cam in can_parsers:
+      self.das_body_controls_dat = bytes(can_parsers[Bus.cam].dat.get(0x3E9, b""))
+
+    # 3-finger infotainment press detection (vehicle bus)
+    if Bus.adas in can_parsers:
+      cp_adas = can_parsers[Bus.adas]
+      prev_infotainment = self.infotainment_3_finger_press
+      self.infotainment_3_finger_press = int(cp_adas.vl["UI_status2"]["UI_activeTouchPoints"])
+      ret.buttonEvents = [*ret.buttonEvents, *create_button_events(
+        self.infotainment_3_finger_press, prev_infotainment,
+        {3: ButtonType.lkas})]
+
+    return ret
+
+  @staticmethod
+  def get_can_parsers(CP):
+    parsers = {
+      Bus.party: CANParser(DBC[CP.carFingerprint][Bus.party], [], CANBUS.party),
+      Bus.ap_party: CANParser(DBC[CP.carFingerprint][Bus.party], [], CANBUS.autopilot_party),
+    }
+    if CP.flags & TeslaFlags.HAS_VEHICLE_BUS:
+      parsers[Bus.adas] = CANParser("tesla_model3_vehicle", [("UI_status2", 2)], CANBUS.vehicle)
+    if CP.flags & TeslaFlags.HAS_DAS_BODY_CONTROLS:
+      parsers[Bus.cam] = CANParser("tesla_model3_vehicle", [("DAS_bodyControls", 2)], CANBUS.autopilot_party)
+    return parsers
