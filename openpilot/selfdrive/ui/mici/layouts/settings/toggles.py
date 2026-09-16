a8926bfa281f3a0280c35afdda1ae1865514bc7f
kim2629 <74346292+kimjk2629@users.noreply.github.com>
2026-09-16 18:48:19 +0900

    Add files via upload

--- openpilot/selfdrive/ui/mici/layouts/settings/toggles.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/toggles.py
@@ -18,6 +18,7 @@ class TogglesLayoutMici(NavScroller):
     is_metric_toggle = BigParamControl("use metric units", "IsMetric")
     ldw_toggle = BigParamControl("lane departure warnings", "IsLdwEnabled")
     always_on_dm_toggle = BigParamControl("always-on driver monitor", "AlwaysOnDM")
+    disable_dm_toggle = BigParamControl("disable driver monitoring", "DisableDM", toggle_callback=restart_needed_callback)
     record_front = BigParamControl("record & upload driver camera", "RecordFront", toggle_callback=restart_needed_callback)
     record_mic = BigParamControl("record & upload mic audio", "RecordAudio", toggle_callback=restart_needed_callback)
     enable_openpilot = BigParamControl("enable sunnypilot", "OpenpilotEnabledToggle", toggle_callback=restart_needed_callback)
@@ -28,6 +29,7 @@ class TogglesLayoutMici(NavScroller):
       is_metric_toggle,
       ldw_toggle,
       always_on_dm_toggle,
+      disable_dm_toggle,
       record_front,
       record_mic,
       enable_openpilot,
@@ -39,6 +41,7 @@ class TogglesLayoutMici(NavScroller):
       ("IsMetric", is_metric_toggle),
       ("IsLdwEnabled", ldw_toggle),
       ("AlwaysOnDM", always_on_dm_toggle),
+      ("DisableDM", disable_dm_toggle),
       ("RecordFront", record_front),
       ("RecordAudio", record_mic),
       ("OpenpilotEnabledToggle", enable_openpilot),
